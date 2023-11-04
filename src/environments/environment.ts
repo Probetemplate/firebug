fb3056b60e7e36681fe250bfe2d029ec1761352c
Probetemplate <147812661+Probetemplate@users.noreply.github.com>
2023-11-04 10:26:47 +0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,13 +7,14 @@ import firebase from 'firebase/compat';
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCNOg5DVxLpiKWSj-EbIE5EDLg1XUxBFJU",
-    authDomain: "github-authentication-75a59.firebaseapp.com",
-    projectId: "github-authentication-75a59",
-    storageBucket: "github-authentication-75a59.appspot.com",
-    messagingSenderId: "890130377781",
-    appId: "1:890130377781:web:d075a9925498be9af5157c",
-    measurementId: "G-EN2G66WCM3"
+    apiKey: "AIzaSyBzzaSTr42firr3fx5YcdXhDzB4iBLVBOM",
+  authDomain: "contact-database-9c47b.firebaseapp.com",
+  databaseURL: "https://contact-database-9c47b-default-rtdb.firebaseio.com",
+  projectId: "contact-database-9c47b",
+  storageBucket: "contact-database-9c47b.appspot.com",
+  messagingSenderId: "118822537955",
+  appId: "1:118822537955:web:3ef795baeadbe37b7293d6",
+  measurementId: "G-0WY0798WFC"
   },
 };
 
