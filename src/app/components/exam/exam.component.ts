fde39990dac6a8a0dbb0c7810890adddd2b8cd7e
Probetemplate <147812661+Probetemplate@users.noreply.github.com>
2023-11-11 21:52:07 +0600

    Update exam.component.ts

--- src/app/components/exam/exam.component.ts
+++ src/app/components/exam/exam.component.ts
@@ -1,1 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
 
+@Component({
+  selector: 'app-exam',
+  templateUrl: './exam.component.html',
+  styleUrls: ['./exam.component.scss'],
+})
+
+export class HomeComponent implements OnInit {
+  constructor(public authService: AuthService) {}
+
+  ngOnInit() {}
+}
