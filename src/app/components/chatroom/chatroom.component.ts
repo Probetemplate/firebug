243102565e9cc2ce629771be58eb84b38c5b60b7
Probetemplate <147812661+Probetemplate@users.noreply.github.com>
2023-11-11 21:26:48 +0600

    Update chatroom.component.ts

--- src/app/components/chatroom/chatroom.component.ts
+++ src/app/components/chatroom/chatroom.component.ts
@@ -1,1 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
 
+@Component({
+  selector: 'app-chatroom',
+  templateUrl: './chatroom.component.html',
+  styleUrls: ['./chatroom.component.scss'],
+})
+
+export class ChatroomComponent implements OnInit {
+  constructor(public authService: AuthService) {}
+
+  ngOnInit() {}
+}
